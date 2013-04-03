0b27ad392e3106854d075b36870a7db3281d6217
Forrest <forrest.shi@ef.com>
2013-04-03 15:30:35 +0800

    save hash value of pwd

--- SecretNote/AppSetting.m
+++ SecretNote/AppSetting.m
@@ -7,8 +7,29 @@
 //
 
 #import "AppSetting.h"
+#import <CommonCrypto/CommonHMAC.h>
 
 @implementation AppSetting
+
+
+
++(NSString *) hashString:(NSString *) data withSalt:(NSString *)salt {
+    
+    const char *cKey  = [salt cStringUsingEncoding:NSUTF8StringEncoding];
+    const char *cData = [data cStringUsingEncoding:NSUTF8StringEncoding];
+    unsigned char cHMAC[CC_SHA256_DIGEST_LENGTH];
+    CCHmac(kCCHmacAlgSHA256, cKey, strlen(cKey), cData, strlen(cData), cHMAC);
+    
+    NSString *hash;
+    
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", cHMAC[i]];
+    hash = output;
+    return hash;
+    
+}
 
 + (BOOL)haveSetupPassword{
     return [[NSUserDefaults standardUserDefaults] boolForKey:@"create_pwd"];
@@ -20,17 +41,19 @@
 }
 
 + (void)savePassword:(NSString*)pwd emailAddress:(NSString*)ea{
-    [[NSUserDefaults standardUserDefaults] setValue:pwd forKey:@"pwd"];
+    
+    [[NSUserDefaults standardUserDefaults] setValue:[AppSetting hashString:pwd withSalt:ea.length > 0 ? ea : @"default_salt"] forKey:@"pwd_hash"];
     [[NSUserDefaults standardUserDefaults] setValue:ea forKey:@"email"];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 + (NSString*)whatisPassword{
-    return [[NSUserDefaults standardUserDefaults] stringForKey:@"pwd"];
+    return [[NSUserDefaults standardUserDefaults] stringForKey:@"pwd_hash"];
 }
 
 + (BOOL)isPasswordCorrect:(NSString*)pwd{
-    if (pwd && [pwd isEqualToString:[AppSetting whatisPassword]]) {
+    NSString *ea = [[NSUserDefaults standardUserDefaults] stringForKey:@"email"];
+    if (pwd && [[AppSetting hashString:pwd withSalt:ea.length > 0 ? ea : @"default_salt"] isEqualToString:[AppSetting whatisPassword] ]) {
         return YES;
     }else{
         return NO;
